996392af64f941ff3d9339c84b22d70200d18668
Ilya <kotyrov.1310@gmail.com>
2023-05-26 00:26:07 +0200

    layout personal data editor form

--- src/components/PersonalTabContent.js
+++ src/components/PersonalTabContent.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
 import './styles/PersonalTabContent.css';
+import PhotoInput from './PhotoInput';
 
 class PersonalTabContent extends Component {
 	state = {};
 
 	render() {
-		return <div className="tab-content">Personal info editor</div>;
+		return (
+			<div className="tab-content">
+				<PhotoInput />
+				<div className="form">
+					<label htmlFor="first-name-input" id="first-name">
+						First Name
+						<input id="first-name-input"></input>
+					</label>
+					<label htmlFor="first-name-input" id="last-name">
+						Last Name
+						<input id="last-name-input"></input>
+					</label>
+					<label htmlFor="title-input" id="title">
+						Professional Title
+						<input id="titl-input"></input>
+					</label>
+					<label htmlFor="address-input" id="address">
+						Street Address
+						<input id="address-input"></input>
+					</label>
+					<label htmlFor="postcode-input" id="postcode">
+						Postcode
+						<input id="postcode-input"></input>
+					</label>
+					<label htmlFor="city-input" id="city">
+						City
+						<input id="city-input"></input>
+					</label>
+					<label htmlFor="phone-input" id="phone">
+						Phone
+						<input id="phone-input"></input>
+					</label>
+					<label htmlFor="email-input" id="email">
+						Email
+						<input id="email-input"></input>
+					</label>
+				</div>
+			</div>
+		);
 	}
 }
 
---
+++ src/components/PhotoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './styles/PhotoInput.css';
+
+class PhotoInput extends Component {
+	state = {};
+	render() {
+		return <div className="photo-input">Upload photo</div>;
+	}
+}
+
+export default PhotoInput;
