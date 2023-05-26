4d0bbc3290e8ce04b2d129a2be5f6ab68dc36df6
Ilya <kotyrov.1310@gmail.com>
2023-05-26 16:44:26 +0200

    add summary editor

--- src/components/SummaryTabContent.js
+++ src/components/SummaryTabContent.js
@@ -4,7 +4,12 @@ import './styles/SummaryTabContent.css';
 class SummaryTabContent extends Component {
 	state = {};
 	render() {
-		return <div className="tab-content">Summary editor</div>;
+		return (
+			<div className="tab-content summary-tab">
+				<h2>Write a short pitch about yourself</h2>
+				<textarea className="summary-input"></textarea>
+			</div>
+		);
 	}
 }
 
